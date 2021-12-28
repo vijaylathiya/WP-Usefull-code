97da1e528c5d106e840b2ed99cfabd1135b441db
vijaylathiya <lathiyavijay.it2009@gmail.com>
2021-12-28 11:22:23 +0530

    WooCommerce Shop - Archive page

--- WC-Shop-category-achive-page.php
+++ WC-Shop-category-achive-page.php
@@ -13,3 +13,23 @@ function custom_pre_get_posts_query( $q ) {
 }
 add_action( 'woocommerce_product_query', 'custom_pre_get_posts_query' ); 
 
+// Show particular Meta value Products only on the Shop and Products Categories archive page
+
+function custom_pre_get_posts_query( $q ) {
+    $meta_query = (array) $q->get( 'meta_query' );
+    $meta_query[] = array(
+            'relation' => 'OR',
+            array(
+                'key'       => 'c_type',
+                'compare'   => '==',
+                'value'     =>  0,
+            ),
+            array(
+                'key'       => 'c_type',
+                'compare'   => 'NOT EXISTS',
+            )
+    );
+    $q->set('meta_query', $meta_query);
+}
+add_action( 'woocommerce_product_query', 'custom_pre_get_posts_query' ); 
+
