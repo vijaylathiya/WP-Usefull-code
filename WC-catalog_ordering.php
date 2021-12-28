56a21ba5b4b21430b55ff74b98d130ac97565a2a
vijaylathiya <lathiyavijay.it2009@gmail.com>
2021-12-28 12:26:26 +0530

    WooCommerce Catalog Orderby Options and Order

--- WC-catalog_ordering.php
+++ WC-catalog_ordering.php
@@ -14,6 +14,20 @@ function wc_customize_product_sorting($sorting_options){
 }
 add_filter('woocommerce_catalog_orderby', 'wc_customize_product_sorting');
 
+// WooCommerce - Change default catalog sort order
+add_filter('woocommerce_default_catalog_orderby', 'custom_default_catalog_orderby');
+function custom_default_catalog_orderby() {
+     return 'price'; // Can also use popularity, date, title ect default optionds or any custom Order added by custom code 
+}
+
+// WooCommerce - Change default catalog sort order for Specific Category only 
+add_filter('woocommerce_default_catalog_orderby', 'custom_default_catalog_orderby');
+function custom_default_catalog_orderby() {
+	if(is_product_category('category_slug or id')){ 
+     	return 'price'; // Can also use popularity, date, title ect default optionds or any custom Order added by custom code 
+	}
+}
+
 
 
 //Display Outofstock product at last and sorting Based on product menu order 
@@ -26,10 +40,11 @@ function bbloomer_sort_by_stock_status_then_alpha( $args ) {
 }
 
 
-// Add custom sorting options (asc/desc) 
+// Add custom sorting options random_list (Random) (asc/desc)   
+// Ref.  https://woocommerce.com/document/custom-sorting-options-ascdesc/
 add_filter( 'woocommerce_get_catalog_ordering_args', 'custom_woocommerce_get_catalog_ordering_args' );
 function custom_woocommerce_get_catalog_ordering_args( $args ) {
-  $orderby_value = isset( $_GET['orderby'] ) ? wc_clean( $_GET['orderby'] ) : apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby' ) );
+	$orderby_value = isset( $_GET['orderby'] ) ? wc_clean( $_GET['orderby'] ) : apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby' ) );
 	if ( 'random_list' == $orderby_value ) {
 		$args['orderby'] = 'rand';
 		$args['order'] = '';
